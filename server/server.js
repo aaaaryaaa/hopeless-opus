b23d88c54f66c2fbcbb6f340cb0d800bf347c345
aaaaryaaa <aarya.masters@gmail.com>
2024-09-29 10:56:47 +0530

    initial

---
+++ server/server.js
@@ -0,0 +1,28 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
