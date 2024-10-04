370f19602f307296f33ef7c9bb7b449aea8b5805
roop1978 <juneja.roopanshi@gmail.com>
2024-10-04 21:49:53 +0530

    merged nikhil1 w roopesh

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -2,13 +2,11 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
-import Hero from "../components/Hero";
-import Gradient from '../Resources/Gradient.svg';
+
+import Gradient from "../Resources/Gradient.svg";
 import ContactForm from "../components/contactform";
 
-
-import FAQ from '../components/FAQ';
-
+import FAQ from "../components/FAQ";
 
 export default function HomePage() {
   const navigate = useNavigate();
@@ -17,22 +15,21 @@ export default function HomePage() {
   const handleLogout = () => {
     localStorage.removeItem("token");
     setUser(null);
-  }
+  };
 
   return (
-<div
-  className="min-h-screen w-screen flex flex-col gap-10 items-center justify-center text-center p-4 bg-black text-gray-100 shadow-inner overflow-hidden"
-  style={{
-    backgroundColor:'' , // Use imported image
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    zIndex: '-1',
-  }}
->
-      
-      <FAQ/>
-      <ContactForm/>
+    <div
+      className="min-h-screen w-screen flex flex-col gap-10 items-center justify-center text-center p-4 bg-black text-gray-100 shadow-inner overflow-hidden"
+      style={{
+        backgroundColor: "", // Use imported image
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        zIndex: "-1",
+      }}
+    >
+      <FAQ />
+      <ContactForm />
       {/* <Hero/> */}
       {/* <Footer/> */}
     </div>
