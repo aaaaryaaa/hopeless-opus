a5db9e9cfd02965cd5bb08213b62a5ad2b0b9867
UtkarshKumarJha <utk.kumarjha@gmail.com>
2024-10-04 19:58:56 +0530

    countdown & cardlslider homepage

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -4,26 +4,28 @@ import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Gradient from '../Resources/Gradient.svg';
 import ContactForm from "../components/contactform";
-
+import Countdown from "../components/Countdown.js";
+import CardSlider from "../components/CardSlider.js";
 
 import FAQ from '../components/FAQ';
 
 
 export default function HomePage() {
   return (
-<div
-  className="min-h-screen w-screen flex flex-col gap-10 items-center justify-center text-center p-4 bg-black text-gray-100 shadow-inner overflow-hidden"
-  style={{
-    backgroundColor:'' , // Use imported image
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    zIndex: '-1',
-  }}
->
-      
-      <FAQ/>
-      <ContactForm/>
+    <div
+      className="min-h-screen w-screen flex flex-col gap-10 items-center justify-center text-center p-4 bg-black text-gray-100 shadow-inner overflow-hidden"
+      style={{
+        backgroundColor: '', // Use imported image
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        zIndex: '-1',
+      }}
+    >
+      <Countdown></Countdown>
+      <CardSlider></CardSlider>
+      <FAQ />
+      <ContactForm />
       {/* <Hero/> */}
       {/* <Footer/> */}
     </div>
