c19bd28172a27173e9c6cba4fa5d54b662139f2b
aaaaryaaa <aarya.masters@gmail.com>
2024-10-05 00:32:23 +0530

    points system added

--- server/models/User.js
+++ server/models/User.js
@@ -27,6 +27,10 @@ const UserSchema = new Schema({
     type: String,
     default: "0001",
   },
+  points: {
+    type: Number,
+    default: 0,
+  },
   sessionId: {
     type: String,
     default: "",
