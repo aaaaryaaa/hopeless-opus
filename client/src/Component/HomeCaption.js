4ff807e6307cb3d6fc14b001c316e34139555569
Pranav Kumar <pranav.sin123@gmail.com>
2024-10-14 21:55:24 +0530

    Homepage content changed

--- client/src/Component/HomeCaption.js
+++ client/src/Component/HomeCaption.js
@@ -2,11 +2,8 @@ import React from 'react'
 
 const CaptionHp = () => {
   return (
-    <div className="">
-    A good design is not only aesthetically pleasing, but also functional.
-    It should solve the problem. A good design is not only aesthetically
-    pleasing, but also functional. It should solve the problem.A good design is not only aesthetically
-    pleasing, but also functional. It should solve the problem.
+    <div className="text-2xl pt-2 mt-0">
+    Hopeless Opus is back and more thrilling than ever! Step into an immersive online experience where every decision shapes your story! In this interactive storytelling event, your choices hold the power to unlock new paths and outcomes as you tackle mini games at step. The fate of the narrative is in your hands—choose wisely, and watch as your unique journey unfolds in real-time. Be ready for twists, turns, and unexpected revelations, where every choice you make matters! 17th-18th October, 2024
     </div>
   )
 }
