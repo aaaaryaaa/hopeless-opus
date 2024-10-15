86ef853f398718b0eb31eea6928ebc91249af362
SavVySIDD <siddhanbaranwal@gmail.com>
2024-10-15 23:53:20 +0530

    Downgraded ;(

--- client/src/BaseUrl.js
+++ client/src/BaseUrl.js
@@ -1,5 +1,5 @@
 // let isProduction = process.env.NODE_ENV;
 
-const BaseUrl = process.env.REACT_APP_PROD_API_URL || 'http://localhost:5000';
+const BaseUrl = process.env.REACT_APP_PROD_API_URL || 'http://localhost:5001';
 
 export default BaseUrl;
